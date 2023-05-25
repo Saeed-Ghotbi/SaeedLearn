b33746d0ac5581f84c39ae6da2a7f5d3305d3c8f
SaeedGhotbi <SaeedGhotbi@outlook.com>
2023-05-25 15:07:24 +0430

    first commit for Clean Architecture with Saeed Ghotbi

---
+++ SaeedLearn.Domain/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SaeedLearn.Domain.Common;
+
+namespace SaeedLearn.Domain
+{
+    public class Category : BaseDomainEntity
+    {
+
+    }
+}
---
+++ SaeedLearn.Domain/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SaeedLearn.Domain.Common;
+
+namespace SaeedLearn.Domain
+{
+    public class Teacher : BaseDomainEntity
+    {
+        public Course Course { get; set; }
+        public int CourseId { get; set; }
+    }
+}
